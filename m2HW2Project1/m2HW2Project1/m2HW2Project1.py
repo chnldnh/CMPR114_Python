d25cf8aa3cb3064f454d710d586916b311306d5a
Chanel Dinh <chnldnh@gmail.com>
2023-02-17 17:35:10 -0800

    Initial commit

---
+++ m2HW2Project1/m2HW2Project1/m2HW2Project1.py
@@ -0,0 +1,16 @@
+
+
+#m2Homework#2 Project#1
+#Chanel Dinh
+#Quarter of the Year
+
+sales=input("What's your name? ")
+
+if sales=='Mark':
+    print("You're Mark")
+elif sales  > 'Mark':
+    print("You're GT Mark")
+elif sales < 'Mark':
+    print("You're LT Mark")
+else:
+    print("You\'re not Mark")
